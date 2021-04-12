ae2c3e5c7316314dea3c07b841179e016f3415ec
SamScherer1 <samscherer33@gmail.com>
2021-04-12 15:29:49 -0400

    squash ProductCommunicationService

--- UXSDKSwiftSample/DefaultLayoutViewController.swift
+++ UXSDKSwiftSample/DefaultLayoutViewController.swift
@@ -28,23 +28,78 @@
 import UIKit
 import DJIUXSDK
 
+let ProductCommunicationServiceStateDidChange = "ProductCommunicationServiceStateDidChange"
+
 // We subclass the DUXRootViewController to inherit all its behavior and add
 // a couple of widgets in the storyboard.
-class DefaultLayoutViewController: DUXDefaultLayoutViewController {
-
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent;
-    }
-    
-    @IBAction func close () {
-        self.dismiss(animated: true) {
-            
-        }
-    }
-    
+class DefaultLayoutViewController: DUXDefaultLayoutViewController, DJISDKManagerDelegate {
     // We are going to add focus adjustment to the default view.
     override func viewDidLoad() {
         super.viewDidLoad()
-        ProductCommunicationService.shared.registerWithProduct()
+        self.registerWithProduct()
+    }
+    
+    //MARK: - Start Registration
+    func registerWithProduct() {
+        guard
+            let path = Bundle.main.path(forResource: "Info", ofType: "plist"),
+            let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject>,
+            let appKey = dict["DJISDKAppKey"] as? String,
+            appKey != "PASTE_YOUR_DJI_APP_KEY_HERE"
+        else {
+                print("\n<<<ERROR: Please add DJI App Key in Info.plist after registering as developer>>>\n")
+                return
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            NSLog("Registering Product with registration ID: \(appKey)")
+            DJISDKManager.registerApp(with: self)
+        }
+    }
+    
+    //MARK: - Start Connecting to Product
+    open func connectToProduct() {
+        NSLog("Connecting to product...")
+        let startedResult = DJISDKManager.startConnectionToProduct()
+        
+        if startedResult {
+            NSLog("Connecting to product started successfully!")
+        } else {
+            NSLog("Connecting to product failed to start!")
+        }
+    }
+    
+//    public func disconnectProduct() {
+//        DJISDKManager.stopConnectionToProduct()
+//
+//        // This is a little cheat because sdkmanager is not properly disconnecting the product.
+//        NotificationCenter.default.post(Notification(name: Notification.Name(rawValue: "ProductCommunicationManagerStateDidChange")))
+//    }
+    
+    //MARK: - DJISDKManagerDelegate
+    func appRegisteredWithError(_ error: Error?) {
+        if error == nil {
+            self.connectToProduct()
+        } else {
+            NSLog("Error Registrating App: \(String(describing: error))")
+        }
+    }
+    
+    func productConnected(_ product: DJIBaseProduct?) {
+        if product != nil {
+            NSLog("Connection to new product succeeded!")
+        }
+    }
+
+    func productDisconnected() {
+        NSLog("Disconnected from product!");
+    }
+    
+    func didUpdateDatabaseDownloadProgress(_ progress: Progress) {
+        print("didUpdateDatabaseDownloadProgress")
+    }
+    
+
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent;
     }
 }
